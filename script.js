ca449a78752819aa8c9b6449e32892d442d51c2d
ArtemKhriapa <art.pzt@gmail.com>
2019-03-13 14:03:27 +0200

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+// Создать функцию, которая принимает объект, меняет местами ключ и значение и возвращает новый объект. {name: 'mykola', age: 56} -> {mykola: 'name', 56: 'age'}
+
+obj = {
+    name: 'mykola',
+    age: 33
+}
+
+function f(obj) {
+
+    for (var key in obj) {
+        var pass = obj.key; 
+        var log = key;
+        obj[key] = log;
+
+    }
+    return obj;
+}
+
+console.log(f(obj));
+
+
